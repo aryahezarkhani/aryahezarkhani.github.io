f5ed0b955dd91c5002207335fa9cf70e9f7781fc
Arya Hezarkhani <aryahezarkhani@gmail.com>
2016-08-29 16:26:49 -0400

    preview

---
+++ hatch-master/NJWEC/tipuesearch/tipuesearch_content.js
@@ -0,0 +1,218 @@
+var tipuesearch = {
+    "pages": [  {
+            "title": "Home",
+            "text": "New Jersey Work Environment Council (WEC) is a membership alliance of labor, environmental, and community organizations working for safe, secure jobs and a healthy, sustainable environment.",
+            "tags": "labor, environment, community, safe jobs, secure jobs, healthy environment, sustainable, coalition, blue green",
+            "url": "http://njwec.org/index.html"
+        },
+
+           {
+            "title": "About",
+            "text": "Information about WEC’s mission, campaigns, accomplishments, board, staff, and affiliations.",
+            "tags": "leadership, board of directors, executive director, campaign organizer, Dan Fatton, Debra Coyle McFadden, Cecelia Gilligan Leto, Jerell Blakeley, Reverend Fletcher Harper, GreenFaith, Elizabeth Smith, Statewide Education Organizing committee, Carmen Martino, Latino Occupational Safety and Health Initiatve, Sean Spiller, New Jersey Education Association, NJEA, Marie Blistan, NJEA, Avery Grant, Concerned citizens Coalition of Long Branch, Diana Crowder, Health Professionals and Allied Employees, HPAE, Colandus 'Kelly' Francis, NAACP, Amy Goldsmith, NJ Clean Water Action, Lou Kimmel, New Labor, Adam Liebtag, Communications Workers of America, CWA, Doug O’Malley, Environment New Jersey, Dominick Marino, Professional Fire Fighters Association of New Jersey, John Pajak, International Brotherhood of Teamsters Local 866, Phillips 66, John Shinn, United Steelworkers Distrct 4, Cheryl Skeete, American Federation of Teachers, AFT, John Tittel, Sierra Club, Rev. Ronald tuff, NJ Black Issues Convention, Jim Young, The Labor Institute, David Tykulsker, Counsel, funders, national links, BlueGreen Alliance, COSH, National Council on Occupational Safety and Health",
+            "url": "http://njwec.org/about.html"
+        },
+
+           {
+            "title": "News",
+            "text": "The latest news articles, op-eds, events and information on WEC’s work.",
+            "tags": "latest news, updates, coverage, stories, media, coverage",
+            "url": "http://njwec.org/news"
+        },
+
+           {
+            "title": "WEConnect",
+            "text": "Check the latest issues of our monthly email newsletter, WEConnect, or sign up to receive the emails directly.",
+            "tags": "newsletter, email, sign-up, digest, news",
+            "url": "http://njwec.org/news/tag/newsletter"
+        },
+
+           {
+            "title": "Press Releases",
+            "text": "Check the latest press releases from WEC.",
+            "tags": "news, media, crude oil train transparency, worker memorial day, OSHA final rule, Unsafe lead levels in schools, CEPA, Whistleblower rights",
+            "url": "http://njwec.org/news/tag/press-releases"
+        },
+
+           {
+            "title": "Op-Eds",
+            "text": "Check the latest opinion editorials from WEC.",
+            "tags": "editorials, opinion pieces, op-eds",
+            "url": "http://njwec.org/news/tag/op-eds"
+        },
+
+           {
+            "title": "WEC in the News",
+            "text": "Check the latest print or broadcast coverage of WEC and our issues.",
+            "tags": "oil train bills, whistleblowers and watchdog employees, Emergency Response Plans, Communities in Chemical Risk Zones, media, coverage",
+            "url": "http://njwec.org/news/tag/in-the-news"
+        },
+
+           {
+            "title": "Events",
+            "text": "WEC workshops, annual awards dinner, annual meetings and other special events.",
+            "tags": "events, fundraisers, awards, calendar",
+            "url": "http://njwec.org/news/tag/events"
+        },
+
+           {
+            "title": "Trainings",
+            "text": "WEC can offer free training, and may also conduct certain trainings on a fee-for-service basis. Click here to find out available workshops. Effective training about avoiding injury can help achieve safer, healthier and more productive workplaces.",
+            "tags": "Safer After Sandy, OSHA, Susan G. Harwood, rebuild safely after Hurricane Sandy, Injury Illness Prevention, training, safer, healthier and more productive workplaces, safety, health, professional development, hazards, workshops, safe, worker",
+            "url": "http://njwec.org/training.html"
+        },
+
+           {
+            "title": "Safer After Sandy",
+            "text": "OSHA-funded initiative to help people rebuild safely after Hurricane Sandy.",
+            "tags": "Safer After Sandy, OSHA, Susan G. Harwood, rebuild safely after Hurricane Sandy, training, mold, reconstruction, resilience, climate change, storms",
+            "url": "http://njwec.org/safety-after-sandy.html"
+        },
+
+           {
+            "title": "Safe Work, Safe Care",
+            "text": "This initiative works to help create and maintain safe, healthy work environments for healthcare workers and patients.",
+            "tags": "coalition, safety, worker, healthcare, needlestick, Safe Patient, handling, workplace violence",
+            "url": "http://njwec.org/swsc.html"
+        },
+
+           {
+            "title": "Preventing Chemical Accidents",
+            "text": " Health, safety and environmental issues in high hazard facilities like chemical plants, oil refineries, water treatment plants, and food processing plants are addressed by this training. OSHA’s Process Safety Management Standard and NJ’s Toxic Catastrophe Prevention Act are also covered.",
+            "tags": "chemical safety, PSM, process, hazards, training, mapping, TCPA, OSHA, toxics",
+            "url": "http://njwec.org/preventing-chemical-accidents.html"
+        },
+
+           {
+            "title": "Take Action",
+            "text": "Find out how to get more involved with WEC campaigns, access resources such as factsheets, reports, our Health and Safety Guide, and see our Public Service Announcements.",
+            "tags": "Stop Chemical Disasters, Respect Our Right to Know, Healthy Schools Now, repair and modernize schools, Schools Development Authority, petition ",
+            "url": "http://njwec.org/news/tag/take-action"
+        },
+
+           {
+            "title": "Petitions",
+            "text": "Sign our latest campaign petitions to stand with workers demanding both jobs and justice. Join us and stand for the environment AND the economy. Take action today.",
+            "tags": "petitions, campaign, sign, join",
+            "url": "http://njwec.org/news/tag/petitions"
+        },
+
+           {
+            "title": "Healthy Schools Now",
+            "text": "WEC chairs a coalition working for safe and modern schools for all children in New Jersey.",
+            "tags": "Healthy Schools, coalition, safety, mold, asthma, temperature control, funding, SDA, Schools Development Authority, Jerell Blakeley, Angelina Carione, education, teachers, students",
+            "url": "http://njwec.org/healthy-schools-now.html"
+        },
+
+           {
+            "title": "Respect Our Right to Know",
+            "text": "WEC and its allies call on state leaders to provide access to Emergency Response Plans and protect our lives, jobs, health, schools and businesses. We firmly believe that the public has a right to know about hazards.",
+            "tags": "right to know, RTK, law, safety, access, transparency, information, training, prevention, planning, chemical, emergency",
+            "url": "http://njwec.org/respect-our-rtk.html"
+        },
+
+           {
+            "title": "Reversing Runaway Inequality",
+            "text": "WEC and its allies are working to address corporate influence on public policy and rein in Wall Street.",
+            "tags": "runaway inequality, economic inequality, one percent, corporate power, Wall St., banks, 99%, Bernie Sanders, Occupy, hedgefund, venture capital, wealth inequality",
+            "url": "http://njwec.org/runaway-inequality.html"
+        },
+
+           {
+            "title": "Climate Policy",
+            "text": " As the nation’s longest standing state labor and environmental coalition, WEC seeks to bring together a powerful group of workers, environmentalists and community members to collaborate and push for meaningful energy and climate policy changes in New Jersey.",
+            "tags": "climate change, clean energy, labor policy, just transition, renewables, solar, wind, NJ, climate, coalition, sea level rise, environment",
+            "url": "http://njwec.org/climate-policy.html"
+        },
+
+           {
+            "title": "Save Our Safeguards",
+            "text": " Efforts to protect and strengthen safeguards for health and safety, and oppose threats to New Jersey’s regulatory process.",
+            "tags": "regulation, safeguards, health, safety, public health, protections",
+            "url": "http://njwec.org/save-our-safeguards.html"
+        },
+
+           {
+            "title": "Resources",
+            "text": " A list of potential resources from WEC on health, safety and other issues.",
+            "tags": "silica, artificial turf, comfortable temperatures, mold, toxic-free schools, tar sands oil, bakken crude oil, protecting emergency responders from Hazards",
+            "url": "http://njwec.org/resources.html"
+        },
+
+           {
+            "title": "Factsheets",
+            "text": "WEC has useful fact sheets on many health and safety issues, including: silica, whistleblower protection, infectious diseases.",
+            "tags": "facts, resource, safety, silica, hazards, protection, research",
+            "url": "http://njwec.org/news/tag/fact-sheets.html"
+        },    {
+            "title": "Reports",
+            "text": " WEC has authored reports, including Danger in the Dark, and Failure to Act, which document the failures of Governor Christie and other state leaders. Reports can be downloaded from our website.",
+            "tags": "facts, research, reports, Christie, Governor, state, chemical, Danger, failure, policy",
+            "url": "http://njwec.org/news/tag/reports"
+        },
+
+           {
+            "title": "Health and Safety Training Guide",
+            "text": " WEC’s Preventing Workplace Hazards: An Action Guide for New Jersey Workers and Unions provides practical advice and resources for solving health and safety problems.",
+            "tags": "state, NJ, hazards, prevention, worker, workplace, action, guide, unions, danger",
+            "url": "http://njwec.org/health-safety-action.html"
+        },
+
+           {
+            "title": "PSAs",
+            "text": " WEC has produced television and radio Public Service Announcements about numerous health and safety issues to educate the public and link people to additional resources.",
+            "tags": "ergonomics, workplace violence, silica, safe workplaces, generator safety, ladder safety, infectious disease, PSA, television, radio",
+            "url": "http://njwec.org/news/tag/psas"
+        },
+
+           {
+            "title": "Support",
+            "text": " Become an individual member, join as an organization, sponsor an event, or donate to support WEC’s work linking workers, communities, and environmentalists through training, technical assistance, grassroots organizing and public policy campaigns to promote dialogue, collaboration, and joint action.",
+            "tags": "Donate, Support, Join, Membership, Give, Donor",
+            "url": "http://njwec.org"
+        },
+
+           {
+            "title": "Donate",
+            "text": "Make a one-time or monthly donation to support WEC’s work linking workers, communities, and environmentalists through training, technical assistance, grassroots organizing and public policy campaigns to promote dialogue, collaboration, and joint action.",
+            "tags": "Donate, Support, Join, Give, Donor",
+            "url": "http://njwec.org"
+        },
+
+           {
+            "title": "Individual Membership",
+            "text": " Join WEC, become an individual member for as little as $10.",
+            "tags": "Support, Join, Membership, Give, Member",
+            "url": "http://njwec.org"
+        },
+
+           {
+            "title": "Organizational Membership",
+            "text": "Join WEC, have your organization become a member to support the nation’s longest standing state based labor and environmental organization, and the New Jersey state affiliate of the Council on Occupational Safety and Health.",
+            "tags": "Support, Join, Membership, Give, Member, Organization",
+            "url": "http://njwec.org"
+        },
+
+           {
+            "title": "Infectious Diseases",
+            "text": " WEC has compiled resources on infectious diseases. Find out about infectious diseases caused by organisms and how healthcare workers are impacted.",
+            "tags": "infectious disease, prevention, healthcare, hospital, nurses.",
+            "url": "http://njwec.org/infectious-disease.html"
+        },
+
+           {
+            "title": "Silica",
+            "text": " WEC has compiled resources on silica. Find out about the new OSHA final rule to improve protections for workers from silica dust.",
+            "tags": "silica, construction, dry cutting, silicosis, OSHA.",
+            "url": "http://njwec.org/silica.html"
+        },
+
+           {
+            "title": "Ergonomics",
+            "text": " WEC has compiled resources on ergonomics. Find out about designing a job to fit the worker.",
+            "tags": "ergonomics, safety, sitting, worker, OSHA.",
+            "url": "http://njwec.org/ergonomics.html"
+        }
+    ]
+};
+ 
